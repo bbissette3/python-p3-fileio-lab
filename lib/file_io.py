bac44cfe8c417999202af8ec7285b5d7620f09cd
Brian Bissette <bbissette3@yahoo.com>
2023-04-05 15:29:44 -0500

    lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,10 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a") as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    return  open(f'{file_name}.txt').read()
